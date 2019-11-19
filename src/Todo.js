c51f4521e97a2e32d8b0bdf811f7d8754efdc99d
Sarahmdawood <51886388+Sarahmdawood@users.noreply.github.com>
2019-11-19 14:53:08 +0500

    Styling

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,19 @@
 import React,{Component} from "react"
+import './Todo.css'
 
 
 class Todo extends Component {
     constructor(props) {
         super(props);
         this.state= {
-            isEditing: false
+            isEditing: false,
+            task: this.props.task
         }
         this.handleRemove = this.handleRemove.bind(this);
         this.toggleForm = this.toggleForm.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
     }
     handleRemove() {
         this.props.removeTodo(this.props.id);
@@ -18,22 +23,52 @@ class Todo extends Component {
             isEditing: !this.state.isEditing
         });
     }
+    handleUpdate(event) {
+        event.preventDefault();
+        //take new task data and pass up to parent
+        this.props.updateTodo(this.props.id,this.state.task);
+        this.setState({
+            isEditing: !this.state.isEditing
+        });
+    }
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    handleToggle (event) {
+        this.props.toggleTodo(this.props.id)
+    }
     render() {
         if(this.state.isEditing) {
             return (
-                <div>
+                <div className="Todo">
                     <form onSubmit={this.handleUpdate}>
-                        <input type="text"></input>
+                        <input 
+                            type="text" 
+                            value={this.state.task} 
+                            name="task" 
+                            onChange={this.handleChange}
+                        />
                         <button>Save</button>
                     </form>
                 </div>
             )
         } else {
             return (
-                <div>
-                    <button onClick={this.toggleForm}>Edit</button>
-                    <button onClick={this.handleRemove}>x</button>
-                    <li>{this.props.task}</li>
+                <div className="Todo">
+                    <li 
+                        onClick={this.handleToggle} 
+                        className={this.props.completed? "Todo-task completed" : "Todo-task"}
+                    >
+                        {this.props.task}
+                    </li>
+                    <div className="Todo-buttons">
+                        <button onClick={this.toggleForm}>
+                            <i class='fas fa-pen'/>
+                        </button>
+                        <button onClick={this.handleRemove}><i class='fas fa-trash'/></button>
+                    </div>
                 </div>
             )
         }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,20 @@
 import React,{Component} from "react"
 import Todo from './Todo'
 import NewTodoForm from "./NewTodoForm";
+import './TodoList.css'
 
 
 class TodoList extends Component{
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            todos: []
+            todos: [],
+            completed:false
          }
          this.create = this.create.bind(this);
          this.remove = this.remove.bind(this);
+         this.update = this.update.bind(this);
+         this.toggleTodo = this.toggleTodo.bind(this);
 
     }
     create(newTodo){
@@ -23,17 +27,43 @@ class TodoList extends Component{
             todos:this.state.todos.filter(t => t.id !== id)
         })
     }
+    update(id, updatedTask) { //What are we updating, update
+        const updatedTodos = this.state.todos.map(todo=> {
+            if(todo.id === id){
+                return{...todo, task:updatedTask}
+            }
+            return todo;
+        });
+        this.setState({
+            todos: updatedTodos
+        })
+    }
+    toggleTodo (id) {
+        this.setState({
+            completed: !this.state.completed
+        })
+    }
     render() {
         const todos = this.state.todos.map(todo=> {
-            return <Todo key={todo.id} id={todo.id} removeTodo={this.remove} task = {todo.task} />
+            return <Todo 
+                key={todo.id} 
+                id={todo.id} 
+                removeTodo={this.remove} 
+                task = {todo.task} 
+                updateTodo = {this.update}
+                completed = {this.state.completed} 
+                toggleTodo = {this.toggleTodo}
+            />
         })
         return (
-            <div>
-                <h1>Todo List!</h1>
-                <NewTodoForm createTodo={this.create}/>
+            <div className="todoList">
+                <h1>React Todo List!<span>A Simple React Todo List</span></h1>
+                
                 <ul>
                    {todos}
                 </ul>
+
+                <NewTodoForm createTodo={this.create}/>
             </div>
         )
     }
